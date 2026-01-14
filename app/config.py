7dfd305ed3da2e8a7ed0587a5f7f95cb0a7ab74a
Tomasz Taraś <tomasztaras@outlook.com>
2026-01-14 23:07:10 +0100

    add some default settings values

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    db_connection_string: str
-    data_dir: str
-    base_url: str
+    db_connection_string: str = "sqlite:///test.db"
+    data_dir: str = "./"
+    base_url: str = "http://127.0.0.1:8000"
 
     model_config = SettingsConfigDict(env_file=".env", env_prefix="temportofoto_")
 
